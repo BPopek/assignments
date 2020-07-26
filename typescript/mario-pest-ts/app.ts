7a5cd6d484906a9bdebb82bece1147ac1b7f9603
BPopek <betsypopek@gmail.com>
2020-07-26 15:03:32 -0600

    mario ts

---
+++ typescript/mario-pest-ts/app.ts
@@ -0,0 +1,15 @@
+const baddieForm = document.baddieForm
+
+
+
+baddieForm.addEventListener('submit', function(){
+    event.preventDefault()
+        const gTotal = (baddieForm.goombaTotal.value * 5)
+        const bTotal = (baddieForm.bobTotal.value * 7)
+        const cTotal = (baddieForm.cheepTotal.value * 11)
+        const total = parseInt(gTotal) + parseInt(bTotal) + parseInt(cTotal)
+console.log(total)
+
+// document.getElementById('baddieTotal').textContent = `${gTotal} + ${bTotal} + ${cTotal}`
+document.getElementById('baddieTotal').textContent = `${total} coins`
+})
